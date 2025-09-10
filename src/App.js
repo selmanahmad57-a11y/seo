02e6d59a6735ad96e52284001aeab8f15240283e
selmanahmad57-a11y <selmanahmad57@gmail.com>
2025-09-10 22:37:53 +0000

    Initial commit: Structure de base du projet SEO Analyzer

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SEOAnalyzer from './components/SEOAnalyzer';
+
+function App() {
+  return (
+    <div className="App">
+      <SEOAnalyzer />
+    </div>
+  );
+}
+
+export default App;
